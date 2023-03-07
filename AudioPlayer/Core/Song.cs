2d46a221039089d5f5a81955541243ea0ab3a8c4
Николай Чернявский <chern.nic@gmail.com>
2023-03-07 12:43:48 +0300

    FF

--- AudioPlayer/Core/Song.cs
+++ AudioPlayer/Core/Song.cs
@@ -4,37 +4,32 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using TagLib;
 
 namespace AudioPlayer.Core
 {
     class Song
     {
-        private string FilePath { get; set; }
-        private TagLib.File File { get; set; }
-        private string SongName { get; set; }
-        private string Album { get; set; }
-        private string Artist { get; set; }
-        private IPicture[] Cover { get; set; }
+        public string FilePath { get; set; }
+        public TagLib.File File { get; set; }
+        public string SongName { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+        public BitmapImage Image { get; set; }
+        public BitmapImage isPlayingImage { get; set; }
+        public IPicture[] Cover { get; set; }
+        public bool isPlaying { get; set; }
 
         public Song(string filePath) 
         {
             FilePath = filePath;
-
             File = TagLib.File.Create(FilePath);
-            if (File.Tag == null)
-            {
-                SongName = Path.GetFileNameWithoutExtension(FilePath);
-                Album = "";
-                Artist = "Unkown Artist";
-            }
-            else
-            {
-                SongName = File.Tag.Title;
-                Artist = File.Tag.FirstAlbumArtist;
-                Album = File.Tag.Album;
-                Cover = File.Tag.Pictures;
-            }
+            SongName = File.Tag.Title ?? Path.GetFileNameWithoutExtension(FilePath);
+            Artist = File.Tag.FirstPerformer ?? "Unkown Artist";
+            Album = File.Tag.Album;
+            isPlaying = false;
+            Cover = File.Tag.Pictures;
         }
     }
 }
